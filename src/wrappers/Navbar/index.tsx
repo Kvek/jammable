55c9de125da51cfbc94c2dee60b77f95bcc66791
vkavek <vkavek.dev@pm.me>
2024-03-21 01:05:02 +0000

    feat: add search icon to navbar

--- src/wrappers/Navbar/index.tsx
+++ src/wrappers/Navbar/index.tsx
@@ -2,21 +2,27 @@ import type { FC } from "react";
 
 import { Logo, ThemeToggle } from "@wrappers";
 
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+
 export const Navbar: FC = () => (
-  <div className="dark:shadow-dark fixed z-[1] flex h-16 w-full items-center shadow">
-    <div className="bg-shadow-rainbow fixed h-5 w-full select-none blur-2xl" />
-    <div className="z-10 flex h-full w-full items-center justify-between bg-background px-2">
-      <div className="flex select-none">
+  <div className="fixed z-[3] flex h-16 w-full items-center shadow dark:shadow-dark">
+    <div className="fixed h-5 w-full select-none bg-shadow-rainbow blur-2xl" />
+    <div className="z-10 flex h-full w-full items-center justify-between bg-background px-4">
+      <div className="flex select-none items-center">
         <Logo />
 
-        <div className="ml-2 text-3xl font-semibold">
+        <div className="ml-2 text-xl font-semibold sm:text-2xl">
           <span className="bg-gradient-to-r from-pink-300 to-purple-700 bg-clip-text text-transparent">
             Jammable
           </span>
         </div>
       </div>
 
-      <ThemeToggle />
+      <span className="flex items-center">
+        <MagnifyingGlassIcon className="mx-2 h-[1.3rem] w-[1.3rem] md:hidden" />
+
+        <ThemeToggle />
+      </span>
     </div>
   </div>
 );
