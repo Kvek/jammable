8a5c0e3d16e96c4f94936ebae33d7a5959ce95f4
vkavek <vkavek.dev@pm.me>
2024-03-21 22:00:56 +0000

    refactor: update navlinks component

--- src/wrappers/Navlinks/index.tsx
+++ src/wrappers/Navlinks/index.tsx
@@ -1,11 +1,7 @@
-import type { RouteType } from "next/dist/lib/load-custom-routes";
-import type { LinkProps } from "next/link";
-import NextLink from "next/link";
-import type { ComponentType, FC } from "react";
+import type { OptionalWrapperComponentType } from "@types";
 
-import type { WrapperComponentType } from "@types";
+import { LinkItem } from "@components/shared/LinkItem";
 
-import { cn } from "@lib/utils";
 import {
   DashboardIcon,
   GlobeIcon,
@@ -13,43 +9,32 @@ import {
   IdCardIcon,
 } from "@radix-ui/react-icons";
 
-const Link: WrapperComponentType<LinkProps<RouteType>> = ({
-  children,
-  ...props
-}) => (
-  <NextLink
-    {...props}
-    className={cn(
-      "flex w-full flex-col items-center justify-center opacity-60 md:mr-4 md:flex-row md:font-semibold md:hover:opacity-100",
-      props.className,
-    )}
-    href={props.href}
-  >
-    {children}
-  </NextLink>
-);
-
-const LinkItem: FC<
-  LinkProps<RouteType> & {
-    Icon: ComponentType<{
-      className?: string;
-    }>;
-    content: string;
-  }
-> = ({ Icon, content, ...props }) => (
-  <li className="flex w-[25%] md:mr-2 md:w-auto">
-    <Link {...props}>
-      <Icon className="flex h-6 w-6 md:mr-1 md:h-5 md:w-5" />
-      <span className="text-[10px] md:text-[14px]">{content}</span>
-    </Link>
-  </li>
-);
-
-export const Navlinks: FC = () => (
+export const Navlinks: OptionalWrapperComponentType = ({ children }) => (
   <ul className="flex h-full w-full items-center justify-between">
-    <LinkItem Icon={HomeIcon} content="Home" href={"/"} />
-    <LinkItem Icon={DashboardIcon} content="My Library" href={"/library"} />
-    <LinkItem Icon={GlobeIcon} content="Community" href={"/community"} />
-    <LinkItem Icon={IdCardIcon} content="Contact" href={"/contact"} />
+    <li className="md:mr-4">
+      <LinkItem Icon={HomeIcon} href={"/"}>
+        <span className="md:ml-1">Home</span>
+      </LinkItem>
+    </li>
+
+    <li className="md:mr-4">
+      <LinkItem Icon={DashboardIcon} href={"/library"}>
+        <span className="md:ml-1">My Library</span>
+      </LinkItem>
+    </li>
+
+    <li className="md:mr-4">
+      <LinkItem Icon={GlobeIcon} href={"/community"}>
+        <span className="md:ml-1">Community</span>
+      </LinkItem>
+    </li>
+
+    <li className="md:mr-4">
+      <LinkItem Icon={IdCardIcon} href={"/contact"}>
+        <span className="md:ml-1">Contact</span>
+      </LinkItem>
+    </li>
+
+    {children}
   </ul>
 );
