d51b609c4d176148b408a0b9589535344890965f
vkavek <vkavek.dev@pm.me>
2024-03-21 19:21:58 +0000

    feat: add navlink to footer

--- src/wrappers/Footer/index.tsx
+++ src/wrappers/Footer/index.tsx
@@ -1,8 +1,12 @@
 import type { FC } from "react";
 
+import { Navlinks } from "@wrappers/Navlinks";
+
 export const Footer: FC = () => (
-  <div className="fixed bottom-0 flex h-12 w-full shadow dark:border-t dark:shadow-dark md:hidden">
+  <div className="fixed bottom-0 flex h-14 w-full shadow dark:border-t dark:shadow-dark md:hidden">
     <div className="fixed bottom-[-0.1rem] h-5 w-full select-none bg-shadow-rainbow blur-xl" />
-    <div className="z-10 flex h-full w-full items-center justify-between bg-background px-2" />
+    <div className="z-10 flex h-full w-full items-center justify-between bg-background px-2">
+      <Navlinks />
+    </div>
   </div>
 );
