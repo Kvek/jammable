8eeafb9c95ff7ed4ff299a7f1831d20179988016
vkavek <vkavek.dev@pm.me>
2024-03-22 09:44:18 +0000

    feat: add new toggle component

---
+++ src/components/ui/toggle.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { cva, type VariantProps } from "class-variance-authority";
+import type { ComponentPropsWithoutRef, ElementRef} from "react";
+import { forwardRef } from "react";
+
+import { cn } from "@/lib/utils";
+import * as TogglePrimitive from "@radix-ui/react-toggle";
+
+const toggleVariants = cva(
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-purple-400 data-[state=on]:text-accent-foreground",
+  {
+    defaultVariants: {
+      size: "default",
+      variant: "default",
+    },
+    variants: {
+      size: {
+        default: "",
+        lg: "h-10 px-3",
+        sm: "h-8 px-2",
+      },
+      variant: {
+        default: "bg-transparent",
+        outline:
+          "border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground",
+      },
+    },
+  },
+);
+
+const Toggle = forwardRef<
+  ElementRef<typeof TogglePrimitive.Root>,
+  ComponentPropsWithoutRef<typeof TogglePrimitive.Root> &
+    VariantProps<typeof toggleVariants>
+>(({ className, size, variant, ...props }, ref) => (
+  <TogglePrimitive.Root
+    className={cn(toggleVariants({ className, size, variant }))}
+    ref={ref}
+    {...props}
+  />
+));
+
+Toggle.displayName = TogglePrimitive.Root.displayName;
+
+export { Toggle, toggleVariants };
