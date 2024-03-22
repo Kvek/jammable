0575fb73b8ad7f946e39e541fa5dcf9e31401f6d
vkavek <vkavek.dev@pm.me>
2024-03-22 17:52:13 +0000

    style: update footer style

--- src/wrappers/Footer/index.tsx
+++ src/wrappers/Footer/index.tsx
@@ -3,9 +3,9 @@ import type { FC } from "react";
 import { Avatar, Navlinks } from "@wrappers";
 
 export const Footer: FC = () => (
-  <div className="dark:shadow-dark fixed bottom-0 flex h-14 w-full shadow dark:border-t md:hidden">
-    <div className="bg-shadow-rainbow fixed bottom-[-0.1rem] h-5 w-full select-none blur-xl" />
-    <div className="z-10 flex h-full w-full items-center justify-between bg-background px-4">
+  <div className="fixed bottom-0 flex h-14 w-full shadow dark:border-t dark:shadow-dark md:hidden">
+    <div className="fixed bottom-[1rem] h-5 w-full select-none bg-shadow-rainbow blur-xl" />
+    <div className="z-[2] flex h-full w-full items-center justify-between bg-background px-4">
       <Navlinks>
         <li />
       </Navlinks>
