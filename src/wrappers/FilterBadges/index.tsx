ef19259de7d155590f2d8d25054fbb8827b86845
vkavek <vkavek.dev@pm.me>
2024-03-23 16:20:18 +0000

    feat: add genre selector to filter badges

--- src/wrappers/FilterBadges/index.tsx
+++ src/wrappers/FilterBadges/index.tsx
@@ -1,29 +1,25 @@
+"use client";
+
 import type { FC } from "react";
+
+import { selectGenresListState } from "@redux/genres/selector";
+import { useAppSelector } from "@redux/store";
 
 import { Badge } from "@components/ui/badge";
 import { Toggle } from "@components/ui/toggle";
 
-const filterData = [
-  "Trending",
-  "Popular",
-  "Music",
-  "Cartoon",
-  "Anime",
-  "Gaming",
-  "German",
-  "Korean",
-  "French",
-  "Public figure",
-];
+export const FilterBadges: FC = () => {
+  const filterData = useAppSelector(selectGenresListState);
 
-export const FilterBadges: FC = () => (
-  <div className="my-2 md:mx-auto md:my-5">
-    {filterData.map((f) => (
-      <Toggle aria-label="Toggle" className="m-[2px]" key={f}>
-        <Badge className="p-1.5" variant="outline">
-          {f}
-        </Badge>
-      </Toggle>
-    ))}
-  </div>
-);
+  return (
+    <div className="my-2 flex flex-wrap justify-center md:my-5">
+      {filterData.map(({ key, title }) => (
+        <Toggle aria-label="Toggle" className="m-[2px]" key={key}>
+          <Badge className="p-1.5" variant="outline">
+            {title}
+          </Badge>
+        </Toggle>
+      ))}
+    </div>
+  );
+};
