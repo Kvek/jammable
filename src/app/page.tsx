a84876340aa4608ca74a54f88da83a9eb9f699b5
vkavek <vkavek.dev@pm.me>
2024-03-26 21:44:54 +0000

    feat: add list blob call to trending componment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,25 @@
-"use client";
-
-import type { FC } from "react";
-
-import { selectGenresListState } from "@redux/genres/selector";
-import { useAppSelector } from "@redux/store";
+import { type FC } from "react";
 
 import { Input } from "@components/ui/input";
 
-import { Banner, FilterBadges, Tiles, Trending } from "@wrappers";
+import { Banner, FilterBadges, Trending } from "@wrappers";
 
-const Home: FC = () => {
-  const genreData = useAppSelector(selectGenresListState);
+const Home: FC = () => (
+  <div className="flex flex-col overflow-x-hidden">
+    <Banner />
 
-  return (
-    <div className="flex flex-col overflow-x-hidden">
-      <Banner />
+    <FilterBadges />
 
-      <FilterBadges />
-
-      <div className="hidden justify-center md:flex">
-        <Input
-          className="border md:max-w-[720px]"
-          placeholder="Search all voices"
-          type="search"
-        />
-      </div>
-
-      <Trending />
-
-      {genreData.map(({ key, title }) => (
-        <Tiles id={key} key={key} title={title} />
-      ))}
+    <div className="hidden justify-center md:flex">
+      <Input
+        className="border md:max-w-[720px]"
+        placeholder="Search all voices"
+        type="search"
+      />
     </div>
-  );
-};
+
+    <Trending />
+  </div>
+);
 
 export default Home;
--- src/wrappers/Trending/index.tsx
+++ src/wrappers/Trending/index.tsx
@@ -12,75 +12,85 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import type { ListBlobResultBlob } from "@vercel/blob";
+import { list } from "@vercel/blob";
 
-const paths = [
-  "/cartoon/image(1).webp",
-  "/cartoon/image(2).webp",
-  "/cartoon/image(3).webp",
-  "/cartoon/image(4).webp",
-  "/cartoon/image(5).webp",
-  "/cartoon/image(6).webp",
-  "/cartoon/image(7).webp",
-  "/cartoon/image(9).webp",
-  "/cartoon/image(10).webp",
-  "/cartoon/image.webp",
-];
+export const Trending = async (): Promise<JSX.Element | null> => {
+  let images: ListBlobResultBlob[] = [];
 
-export const Trending = (): JSX.Element => (
-  <div className="flex w-full flex-col overflow-hidden">
-    <div>
-      <h1 className="mb-2 select-none text-lg font-semibold capitalize opacity-80 md:text-3xl">
-        Trending
-      </h1>
+  try {
+    const { blobs } = await list({
+      mode: "folded",
+      prefix: "trending/",
+    });
+
+    images = blobs.filter(({ size }) => !!size);
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (!images.length) return null;
+
+  return (
+    <div className="flex w-full flex-col overflow-hidden">
+      <div>
+        <h1 className="mb-2 select-none text-lg font-semibold capitalize opacity-80 md:text-3xl">
+          Trending
+        </h1>
+      </div>
+
+      <div className="my-2 h-[425px] w-full md:my-5">
+        <Carousel
+          className="relative flex h-full w-full items-center"
+          opts={{ align: "start", loop: true }}
+        >
+          <CarouselPrevious className="mr-4" />
+
+          <CarouselContent className="h-full w-full">
+            {images.map(({ url }, index) => (
+              <CarouselItem key={index}>
+                <Card className="relative flex h-full w-full overflow-hidden p-4 md:mx-1.5 md:w-[375px] md:p-6">
+                  <div
+                    className="absolute bottom-0 left-0 right-0 top-0 bg-cover blur-xl"
+                    style={{ backgroundImage: `url('${url}')` }}
+                  />
+
+                  <CardContent className="z-[2] flex h-full w-full flex-col gap-2 p-0">
+                    <div className="flex h-full w-full flex-[4] flex-col md:flex-1">
+                      <div className="relative h-full w-full">
+                        <Image
+                          fill
+                          priority
+                          alt="img"
+                          className="object-cover"
+                          src={url}
+                        />
+                      </div>
+                      <div className="w-full">
+                        <TileContent title={index.toString()} />
+                      </div>
+                    </div>
+
+                    <div className="flex-col items-start gap-2 bg-opacity-50">
+                      <div className="flex w-full">
+                        <Button
+                          className="w-full bg-purple-800 p-2 font-semibold text-white hover:bg-purple-700"
+                          variant={"shell"}
+                        >
+                          <AudioLinesIcon className="mr-1" />
+                          Jam with
+                        </Button>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+
+          <CarouselNext className="ml-4" />
+        </Carousel>
+      </div>
     </div>
-
-    <div className="my-2 h-[425px] w-full md:my-5">
-      <Carousel
-        className="relative flex h-full w-full items-center"
-        opts={{ align: "start", loop: true }}
-      >
-        <CarouselPrevious className="mr-4" />
-
-        <CarouselContent className="h-full w-full">
-          {paths.map((_, index) => (
-            <CarouselItem key={index}>
-              <Card className="relative flex h-full w-full overflow-hidden bg-gray-400 p-4 md:mx-1.5 md:w-[375px] md:p-6">
-                <div className="absolute bottom-0 left-0 right-0 top-0 bg-[url('/goku.webp')] bg-cover blur-xl" />
-
-                <CardContent className="z-[2] flex h-full w-full flex-col gap-2 p-0">
-                  <div className="flex h-full w-full flex-[4] flex-col md:flex-1">
-                    <div className="relative h-full w-full">
-                      <Image
-                        fill
-                        alt="goku"
-                        className="object-cover"
-                        src="/goku.webp"
-                      />
-                    </div>
-                    <div className="w-full">
-                      <TileContent title={index.toString()} />
-                    </div>
-                  </div>
-
-                  <div className="flex-col items-start gap-2 bg-opacity-50">
-                    <div className="flex w-full">
-                      <Button
-                        className="w-full bg-purple-800 p-2 font-semibold text-white hover:bg-purple-700"
-                        variant={"shell"}
-                      >
-                        <AudioLinesIcon className="mr-1" />
-                        Jam with Goku
-                      </Button>
-                    </div>
-                  </div>
-                </CardContent>
-              </Card>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-
-        <CarouselNext className="ml-4" />
-      </Carousel>
-    </div>
-  </div>
-);
+  );
+};
